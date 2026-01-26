0b72355f7bb8422a702838a3f3ed0c9c22285fe9
lrsholanda <lrsholanda@gmail.com>
2026-01-26 19:13:46 -0300

    Adicionando CORS para permitir conexão do front

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+// Middleware de CORS simples
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.get("/api", (req, res) => {
   res.json({ mensagem: "Olá, os robôs estão conectados!" });
 });
