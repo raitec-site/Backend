39122db9fd8c926ab38458b2a972648508dc0697
lrsholanda <lrsholanda@gmail.com>
2026-04-21 10:21:49 -0300

    feat: raipedia

--- routes/raipedia/raipedia.js
+++ routes/raipedia/raipedia.js
@@ -1,8 +1,41 @@
 const express = require("express");
 const router = express.Router();
 
+// DADOS MOCK
+const artigos = [
+  {
+    nome: "Capacitação de Arduino",
+    slug: "capacitacao-de-arduino",
+    conteudo: "blábláblá..."
+  },
+  {
+    nome: "Capacitação de SCRUM",
+    slug: "capacitacao-de-scrum",
+    conteudo: "blábláblá..."
+  },
+  {
+    nome: "Capacitação de FLUTTER",
+    slug: "capacitacao-de-flutter",
+    conteudo: "blábláblá..."
+  }
+];
+
+// LISTA DE ARTIGOS
 router.get("/raipedia", (req, res) => {
-  res.render("raipedia/index");
+  res.render("raipedia/index", { artigos });
+});
+
+// ARTIGO INDIVIDUAL
+router.get("/raipedia/:slug", (req, res) => {
+  const slug = req.params.slug;
+
+  const artigo = artigos.find(a => a.slug === slug);
+
+  if (!artigo) {
+    return res.status(404).send("Artigo não encontrado!");
+  }
+
+  res.render("raipedia/artigo", { artigo });
 });
 
 module.exports = router;
