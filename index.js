c3c14c62cd0fee1c788607a1081c2d520199590b
sitedoraitec <sitedoraitec@gmail.com>
2026-05-10 13:09:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ app.use(session({
 // IMPORTAÇÃO DAS ROTAS
 
 const sobreRoutes = require("./routes/quem-somos/sobre");
+
+/*
 const estruturaRoutes = require("./routes/quem-somos/estrutura");
 const membrosRoutes = require("./routes/quem-somos/membros/membros");
 const projetosRoutes = require("./routes/projetos/projetos");
@@ -45,9 +47,11 @@ const testeRoutes = require("./routes/teste");
 const authRoutes = require("./routes/admin/auth");
 const adminRoutes = require("./routes/admin/admin");
 const adminMembrosRoutes = require("./routes/admin/membros");
+*/
 
 // USAR ROTAS
 app.use("/", sobreRoutes);
+/*
 app.use("/", estruturaRoutes);
 app.use("/", membrosRoutes);
 app.use("/", projetosRoutes);
@@ -61,7 +65,7 @@ app.use("/", authRoutes);
 app.use("/", adminRoutes);
 app.use("/", testeRoutes);
 app.use("/admin", adminMembrosRoutes);
-
+*/
 // ROTA PRINCIPAL
 app.get("/", (req, res) => {
   res.render("index");
