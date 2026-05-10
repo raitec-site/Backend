cae37151b4167635c7fc52bb66a68abc0ab3f3aa
sitedoraitec <sitedoraitec@gmail.com>
2026-05-10 13:25:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -31,29 +31,25 @@ app.use(session({
 
 const sobreRoutes = require("./routes/quem-somos/sobre");
 const estruturaRoutes = require("./routes/quem-somos/estrutura");
-/*
 const membrosRoutes = require("./routes/quem-somos/membros/membros");
 const projetosRoutes = require("./routes/projetos/projetos");
 const eventosRoutes = require("./routes/eventos/eventos");
 const extensaoRoutes = require("./routes/extensao/extensao");
 const raipediaRoutes = require("./routes/raipedia/raipedia");
-
 const processoSeletivoRoutes = require("./routes/processo-seletivo/processo-seletivo");
 const noticiasRoutes = require("./routes/noticias/noticias");
 const contatoRoutes = require("./routes/contato/contato");
 const testeRoutes = require("./routes/teste");
 
-
 const authRoutes = require("./routes/admin/auth");
 const adminRoutes = require("./routes/admin/admin");
 const adminMembrosRoutes = require("./routes/admin/membros");
-*/
+
 
 // USAR ROTAS
 app.use("/", sobreRoutes);
 
 app.use("/", estruturaRoutes);
-/*
 app.use("/", membrosRoutes);
 app.use("/", projetosRoutes);
 app.use("/", eventosRoutes);
@@ -66,7 +62,7 @@ app.use("/", authRoutes);
 app.use("/", adminRoutes);
 app.use("/", testeRoutes);
 app.use("/admin", adminMembrosRoutes);
-*/
+
 // ROTA PRINCIPAL
 app.get("/", (req, res) => {
   res.render("index");
