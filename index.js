0eb9d3f3ee1dabd1f717dd8a729f5b7be7259184
LlSSA <95726877+LlSSA@users.noreply.github.com>
2026-05-10 12:42:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(session({
 // =====================
 // ROTAS PÚBLICAS
 // =====================
-/*
+
 const sobreRoutes = require("./routes/quem-somos/sobre");
 const estruturaRoutes = require("./routes/quem-somos/estrutura");
 const membrosRoutes = require("./routes/quem-somos/membros/membros");
@@ -47,20 +47,10 @@ const contatoRoutes = require("./routes/contato/contato");
 const testeRoutes = require("./routes/teste");
 
 
-// =====================
 // ROTAS ADMIN / AUTENTICAÇÃO
-// =====================
-
 const authRoutes = require("./routes/admin/auth");
 const adminRoutes = require("./routes/admin/admin");
 const adminMembrosRoutes = require("./routes/admin/membros");
-
-
-// =====================
-// USO DAS ROTAS (CORRIGIDO)
-// =====================
-
-// 🔥 IMPORTANTE: cada uma com sua base correta
 
 app.use("/sobre", sobreRoutes);
 app.use("/estrutura", estruturaRoutes);
@@ -79,18 +69,14 @@ app.use("/admin", adminRoutes);
 app.use("/admin", adminMembrosRoutes);
 app.use("/", authRoutes);
 
-*/
-// =====================
+
 // ROTA PRINCIPAL
-// =====================
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 
-// =====================
 // START SERVER
-// =====================
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
