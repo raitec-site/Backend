02c91dc162167cb4535a9e3c4a5edf3e2a082e5e
sitedoraitec <sitedoraitec@gmail.com>
2026-05-10 14:04:40 -0300

    Update firebaseAdmin.js

--- services/firebaseAdmin.js
+++ services/firebaseAdmin.js
@@ -1,21 +1,17 @@
 const admin = require("firebase-admin");
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+if (!privateKey) {
+  throw new Error("FIREBASE_PRIVATE_KEY não definida no ambiente");
+}
+
 admin.initializeApp({
-
   credential: admin.credential.cert({
-
-    projectId:
-      process.env.FIREBASE_PROJECT_ID,
-
-    clientEmail:
-      process.env.FIREBASE_CLIENT_EMAIL,
-
-    privateKey:
-      process.env.FIREBASE_PRIVATE_KEY
-        .replace(/\\n/g, '\n')
-
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: privateKey.replace(/\\n/g, '\n')
   })
-
 });
 
 module.exports = admin.firestore();
