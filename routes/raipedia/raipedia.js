922be41327efe79eb2d9b3053045d28665ce5777
sitedoraitec <sitedoraitec@gmail.com>
2026-05-10 14:23:03 -0300

    Update raipedia.js

--- routes/raipedia/raipedia.js
+++ routes/raipedia/raipedia.js
@@ -1,7 +1,7 @@
-const express = require("express");
+/*const express = require("express");
 const router = express.Router();
 const db = require("../../services/firebaseAdmin");
-
+*/
 /** 
 // DADOS MOCK
 const artigos = [
