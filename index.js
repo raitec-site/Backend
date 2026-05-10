61d26e405db3f4162d8382260f51909f4b51d389
sitedoraitec <sitedoraitec@gmail.com>
2026-05-10 13:11:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -31,13 +31,14 @@ app.use(session({
 
 const sobreRoutes = require("./routes/quem-somos/sobre");
 
-/*
+
 const estruturaRoutes = require("./routes/quem-somos/estrutura");
 const membrosRoutes = require("./routes/quem-somos/membros/membros");
 const projetosRoutes = require("./routes/projetos/projetos");
 const eventosRoutes = require("./routes/eventos/eventos");
 const extensaoRoutes = require("./routes/extensao/extensao");
 const raipediaRoutes = require("./routes/raipedia/raipedia");
+/*
 const processoSeletivoRoutes = require("./routes/processo-seletivo/processo-seletivo");
 const noticiasRoutes = require("./routes/noticias/noticias");
 const contatoRoutes = require("./routes/contato/contato");
@@ -51,13 +52,14 @@ const adminMembrosRoutes = require("./routes/admin/membros");
 
 // USAR ROTAS
 app.use("/", sobreRoutes);
-/*
+
 app.use("/", estruturaRoutes);
 app.use("/", membrosRoutes);
 app.use("/", projetosRoutes);
 app.use("/", eventosRoutes);
 app.use("/", extensaoRoutes);
 app.use("/", raipediaRoutes);
+/*
 app.use("/", processoSeletivoRoutes);
 app.use("/", noticiasRoutes);
 app.use("/", contatoRoutes);
